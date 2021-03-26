4ced474671eccadfa3950b1d14d48f08ce30c5d4
Eduard Castellanos <andrescastellanos112013@gmail.com>
2021-03-26 00:01:42 -0500

    chore: The handler has been updated.

--- src/handler.ts
+++ src/handler.ts
@@ -2,11 +2,9 @@ import 'dotenv/config';
 import 'reflect-metadata';
 import { ApolloServer } from 'apollo-server-lambda';
 import { resolvers } from '@resolvers';
-import typeDefs from '@schema';
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+  schema: resolvers,
 });
 
 export const index = server.createHandler({
