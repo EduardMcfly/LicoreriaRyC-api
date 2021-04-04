5b952d7b51a7b16bafed7ca6b15592eedc205596
Eduard Castellanos <andrescastellanos112013@gmail.com>
2021-04-04 17:28:24 -0500

    chore: The handler has improved

--- src/handler.ts
+++ src/handler.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import 'reflect-metadata';
+import { APIGatewayProxyHandler } from 'aws-lambda';
 import { ApolloServer } from 'apollo-server-lambda';
 import { resolvers } from '@resolvers';
 
@@ -7,9 +8,20 @@ const server = new ApolloServer({
   schema: resolvers,
 });
 
-export const index = server.createHandler({
+const handler = server.createHandler({
   cors: {
     origin: true,
     credentials: true,
   },
+  uploadsConfig: { maxFileSize: 10000, maxFiles: 10 },
 });
+
+export const index: APIGatewayProxyHandler = (
+  event,
+  context,
+  callback,
+) => {
+  const { headers } = event;
+  event.headers['content-type'] = headers['Content-Type'];
+  return handler(event, context, callback);
+};
