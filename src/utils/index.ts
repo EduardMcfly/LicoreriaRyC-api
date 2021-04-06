81d62b933b8378381c71a8263e00f96eec14861d
Eduard Castellanos <andrescastellanos112013@gmail.com>
2021-04-06 11:55:30 -0500

    chore(utils): uploadS3 has been improved

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,12 +1,15 @@
+import { Body } from 'aws-sdk/clients/s3';
 import { awsS3, BUCKET } from './awsS3';
+
 interface UploadParams<T = string> {
   key: string;
   file: T;
 }
+
 export const uploadS3 = ({
   key, // File name you want to save as in S3
   file,
-}: UploadParams<Buffer>) =>
+}: UploadParams<Body>) =>
   awsS3
     .upload({
       Bucket: BUCKET,
