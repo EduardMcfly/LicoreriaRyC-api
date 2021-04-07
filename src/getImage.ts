55ffb95b2e6a2ff871621e4c9a62633e00b7d7c6
Eduard Castellanos <andrescastellanos112013@gmail.com>
2021-04-07 00:12:14 -0500

    chore: The getImage handler has been improved.

--- src/getImage.ts
+++ src/getImage.ts
@@ -6,16 +6,16 @@ import resize from 'utils/resize';
 import { getFileBucket, convertInt } from 'utils';
 
 export const getImage: APIGatewayProxyHandler = async ({
-  queryStringParameters: parameters,
+  queryStringParameters: query,
 }): Promise<APIGatewayProxyResult> => {
-  if (!parameters)
+  if (!query)
     return { statusCode: 400, body: 'Parameters undefined' };
-  const { file } = parameters;
-  const format = parameters.format as any;
+  const { file } = query;
+  const format = query.format as any;
 
   // Parse to integer if possible
-  const width = convertInt(parameters.width);
-  const height = convertInt(parameters.height);
+  const width = convertInt(query.width);
+  const height = convertInt(query.height);
 
   if (!file) return { statusCode: 500, body: 'Not autorized' };
   else {
@@ -31,10 +31,17 @@ export const getImage: APIGatewayProxyHandler = async ({
         height,
       })
         .then(
-          ({ data }): APIGatewayProxyResult => ({
-            statusCode: 200,
-            body: JSON.stringify({ file: data.toString('base64') }),
-          }),
+          ({ data, info }): APIGatewayProxyResult => {
+            const content = `image/${info.format}`;
+            return {
+              statusCode: 200,
+              headers: {
+                'Content-type': content,
+              },
+              body: data.toString('base64'),
+              isBase64Encoded: true,
+            };
+          },
         )
         .catch(
           (e): APIGatewayProxyResult => {
