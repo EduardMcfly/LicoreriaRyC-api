1bfdbb2ebb398ed0fec809108468dc4d0cbe8621
Eduard Castellanos <andrescastellanos112013@gmail.com>
2021-05-23 09:04:36 -0500

    chore: Has been removed the resolver bye

--- src/resolvers/hello.ts
+++
@@ -1,9 +0,0 @@
-import { Resolver, Query } from 'type-graphql';
-@Resolver()
-class HelloResolver {
-  @Query(() => String)
-  hello() {
-    return 'Hello world!';
-  }
-}
-export { HelloResolver };
