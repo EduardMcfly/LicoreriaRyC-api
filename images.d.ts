a9d62a71032dfa6443a3fe739a6563e4e6de4cec
Eduard Castellanos <andrescastellanos112013@gmail.com>
2021-05-25 09:47:58 -0500

    chore(images.d.ts): CATEGORY_TABLE added to ProcessEnv

--- images.d.ts
+++ images.d.ts
@@ -2,6 +2,7 @@ declare namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV: 'development' | 'production' | 'test';
     DYNAMODB_TABLE: string;
+    CATEGORY_TABLE: string;
     BUCKET: string;
     CORS: string;
   }
