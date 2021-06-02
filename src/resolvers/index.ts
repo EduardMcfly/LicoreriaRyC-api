74bee55dbb35a5e818e76b32fa7d32e42fd4d354
Eduard Castellanos <andrescastellanos112013@gmail.com>
2021-06-01 20:05:54 -0500

    chore: The resolver CategoryResolver and CartResolver are imported

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,7 +1,9 @@
 import { buildSchemaSync } from 'type-graphql';
 
 import { ProductResolver } from './product';
+import { CategoryResolver } from './category';
+import { CartResolver } from './cart';
 
 export const resolvers = buildSchemaSync({
-  resolvers: [ProductResolver],
+  resolvers: [ProductResolver, CategoryResolver, CartResolver],
 });
