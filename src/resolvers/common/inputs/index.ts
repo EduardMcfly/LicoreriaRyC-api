07230c100ea30f2117f479ef241dd767ac34d924
Eduard Castellanos <andrescastellanos112013@gmail.com>
2021-06-03 15:11:24 -0500

    chore(resolvers/common): Has been added the AfterInput

--- src/resolvers/common/inputs/index.ts
+++ src/resolvers/common/inputs/index.ts
@@ -3,11 +3,28 @@ import { InputType, Field, Int } from 'type-graphql';
 import { OrderTypes } from '../../../constants';
 
 @InputType()
+export class AfterInput {
+  @Field(() => String, { nullable: true })
+  id?: string;
+
+  @Field(() => String, { nullable: true })
+  name?: string;
+
+  @Field(() => String, { nullable: true })
+  categoryId?: string;
+}
+
+@InputType()
 export class Pagination {
   @Field({ nullable: true })
   after?: string;
+
+  @Field(() => [AfterInput], { nullable: true })
+  arrayAfter?: AfterInput[];
+
   @Field(() => OrderTypes, { nullable: true })
   direction!: OrderTypes;
+
   @Field(() => Int, { nullable: true })
   limit!: number;
 }
