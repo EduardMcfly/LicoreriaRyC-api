1d99b66b8239d19d07980d90387792151a6d3310
Eduard Castellanos <andrescastellanos112013@gmail.com>
2021-06-03 15:14:10 -0500

    chore(resolvers/common): ObjectType After has been added

--- src/resolvers/common/objectTypes/index.ts
+++ src/resolvers/common/objectTypes/index.ts
@@ -1,9 +1,21 @@
-import { Field, ObjectType, Int } from 'type-graphql';
+import { Field, Int, ObjectType } from 'type-graphql';
+
+@ObjectType()
+export class After {
+  @Field(() => String, { nullable: true })
+  id?: string;
+  @Field(() => String, { nullable: true })
+  name?: string;
+  @Field(() => String, { nullable: true })
+  categoryId?: string;
+}
 
 @ObjectType()
 export class PageInfo {
   @Field({ nullable: true })
   after?: string;
+  @Field(() => [After], { nullable: 'itemsAndList' })
+  arrayAfter?: (After | undefined)[];
   @Field(() => Int)
   count!: number;
 }
