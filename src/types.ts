66f9ae48100695269778be7ef753fab0f22da0ae
Eduard Castellanos <andrescastellanos112013@gmail.com>
2021-06-09 21:09:12 -0500

    chore(types): The RequestContext has been updated

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,9 @@
 import { Context } from 'aws-lambda';
 
 import * as loaders from './loaders';
+import * as entities from './entities';
 
 export interface RequestContext extends Context {
   loaders: typeof loaders;
+  entities: typeof entities;
 }
